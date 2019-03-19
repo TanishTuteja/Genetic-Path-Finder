84fb90c349c7b72fbc9db3ffabe7254fd4473a2e
TanishTuteja <tanishtuteja@gmail.com>
2019-03-19 12:45:42 +0530

    added dynamic mutation rate based on user input

--- src/javascript/sketch.js
+++ src/javascript/sketch.js
@@ -2,11 +2,11 @@ function setup() {
   createCanvas(windowWidth / 2, windowHeight);
 
   this.lifetime = 150;
-  this.popSize = 100;
-  this.mutationRate = 0.01;
+  this.defaultPopSize = 100;
+  this.defaultMutationRate = 0.01;
   this.genPop = true;
 
-  this.population = new Population(this.popSize, this.lifetime, this.mutationRate);
+  this.population = new Population(this.defaultPopSize, this.lifetime, this.defaultMutationRate);
   this.framec = 0;
 
   this.target = createVector(width / 2, 50);
@@ -23,15 +23,26 @@ function setup() {
 
   this.headerDiv = createDiv("<h3><center> Modify Parameters </center></h3>");
   this.headerDiv.position(windowWidth / 2, 40);
+
   this.popSizeDiv = createDiv();
   popSizeDiv.position(windowWidth / 2, 100);
-  this.popSizeSpan = createSpan("Population Size (Applicable from next generation): ");
+  this.popSizeSpan = createSpan("Population Size (Applicable from next generation): <br>");
   popSizeSpan.parent(popSizeDiv);
-  this.popSizeSlider = createSlider(1, 500, this.popSize, 1);
+  this.popSizeSlider = createSlider(1, 500, this.defaultPopSize, 1);
   this.popSizeSlider.style("margin-left", "10px");
   this.popSizeSlider.parent(popSizeDiv);
-  this.popSizeDisplay = createSpan(this.popSize.toString());
+  this.popSizeDisplay = createSpan(this.defaultPopSize.toString());
   this.popSizeDisplay.parent(popSizeDiv);
+
+  this.mutationRateDiv = createDiv();
+  mutationRateDiv.position(windowWidth / 2, 200);
+  this.mutationRateSpan = createSpan("Mutation Rate: <br>");
+  mutationRateSpan.parent(mutationRateDiv);
+  this.mutationRateSlider = createSlider(0, 1, this.defaultMutationRate, 0.001);
+  this.mutationRateSlider.style("margin-left", "10px");
+  this.mutationRateSlider.parent(mutationRateDiv);
+  this.mutationRateDisplay = createSpan(this.defaultMutationRate.toString());
+  this.mutationRateDisplay.parent(mutationRateDiv);
 
 }
 
@@ -45,8 +56,8 @@ function draw() {
 
   } else if (this.genPop) {
 
-    this.popSize = this.popSizeSlider.value();
-    this.population.nextGen(this.target, this.popSize);
+    this.population.mutationRate = this.mutationRateSlider.value();
+    this.population.nextGen(this.target, this.popSizeSlider.value());
     //this.genPop = !this.genPop;
     this.framec = 0;
 
@@ -66,6 +77,7 @@ function draw() {
   }
 
   this.popSizeDisplay.html(this.popSizeSlider.value().toString());
+  this.mutationRateDisplay.html(this.mutationRateSlider.value().toString());
 
 }
 
